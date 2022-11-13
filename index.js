3bd9dd0f74a302dd9ae2268896c89bf439a1268b
stockromance <94761827+stockromance@users.noreply.github.com>
2022-11-13 18:09:01 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -3,28 +3,6 @@ const codigo = document.querySelector('#codigo');
 codigo.addEventListener('keydown', function(e)
 {
     var key = e.keyCode; 
+    alert(key);
 
-    if(key == 13) 
-    {
-        //tecla enter
-        alert('enter');
-    }
-    else if(key == 9) 
-    {
-        //tecla tab
-        e.preventDefault();
-        alert('tab');
-    }
-    else if((key >= 48 && key <= 57) || (key >= 96 && key <= 105))
-    {
-        //numeros: teclado y teclado numerico   
-    }    
-    else if(key == 37 || key == 39 || key == 8 || key == 46 || key == 9)
-    {
-        //izquierda, derecha, suprimir, borrar, tab
-    }
-    else
-    {        
-        e.preventDefault();
-    }
 });
